9c3b42976a396f7d8471775cd7987297389a3d2d
Mauro Alderete <131892094+MauroAldereteAndroid@users.noreply.github.com>
2023-08-28 23:14:17 -0300

    Add navigation component

---
+++ app/src/main/java/com/maurodev/slottdd/presenters/home/home/HomeFragment.kt
@@ -0,0 +1,32 @@
+package com.maurodev.slottdd.presenters.home.home
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.maurodev.slottdd.databinding.FragmentHomeBinding
+
+class HomeFragment : Fragment() {
+
+    private var _binding: FragmentHomeBinding? = null
+    private val binding get() = _binding!!
+    private lateinit var _vm: HomeViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _vm = ViewModelProvider(this)[HomeViewModel::class.java]
+        _binding = FragmentHomeBinding.inflate(inflater, container, false)
+
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
---
+++ app/src/main/java/com/maurodev/slottdd/presenters/home/home/HomeViewModel.kt
@@ -0,0 +1,7 @@
+package com.maurodev.slottdd.presenters.home.home
+
+import androidx.lifecycle.ViewModel
+
+class HomeViewModel : ViewModel() {
+
+}
